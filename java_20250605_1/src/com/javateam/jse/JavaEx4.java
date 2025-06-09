48190c0d6870259397a3198f2092553d018f84fa
TAEJOON <hazukinoharuka@gmail.com>
2025-06-09 09:37:04 +0900

    Add files via upload

---
+++ java_20250605_1/src/com/javateam/jse/JavaEx4.java
@@ -0,0 +1,14 @@
+package com.javateam.jse;
+
+public class JavaEx4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 6;
+		char ch = '講';
+		String str = "祝日";
+		System.out.printf("6月%d日は%sです。" + "休%c日です。", num1, str, ch);
+		System.out.println("6月" + num1 + "日は" + str + "です。　休" + ch + "講日です。");
+	}
+
+}
